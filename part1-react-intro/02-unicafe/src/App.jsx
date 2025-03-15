8c7695c8db508b75ec384aec8be7b2da345c20cd
Mohammad Talim <talimmohammad116@gmail.com>
2025-03-15 15:49:31 +0530

    start the unicafe project and finish task 1

---
+++ part1-react-intro/02-unicafe/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  return (
+    <main>
+      <h1>Give Feedback</h1>
+
+      <div>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
+      </div>
+
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
