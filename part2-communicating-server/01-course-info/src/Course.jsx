81f85884a83f61df04bbe9ad26cff5b3e8445391
Mohammad Talim <talimmohammad116@gmail.com>
2025-03-19 14:37:45 +0530

    feat: update `App` and `Course` components to support multiple courses
    Finish steps 9 & 10 of course info project

--- part2-communicating-server/01-course-info/src/Course.jsx
+++ part2-communicating-server/01-course-info/src/Course.jsx
@@ -1,21 +1,21 @@
 const Header = (props) => {
-  return <h1>{props.title}</h1>;
+  return <h2>{props.title}</h2>;
 };
 
-const Part = (props) => {
+const Part = ({ name, exercises }) => {
   return (
     <p>
-      {props.name} {props.exercises}
+      {name} {exercises}
     </p>
   );
 };
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
     </>
   );
 };
