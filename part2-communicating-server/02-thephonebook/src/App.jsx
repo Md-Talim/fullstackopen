a9da5e85f06eac17cc2d53a90d8458eab733e3fa
Mohammad Talim <talimmohammad116@gmail.com>
2025-04-05 23:03:03 +0530

    feat: adding names to the phonebook
    Finished exercise 2.6: The Phonebook Step 1

---
+++ part2-communicating-server/02-thephonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addName = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) => (
+          <div key={index}>
+            <p>{person.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
